e2383c8cad0c721a7b7e63c0584c3cc3733e124b
Link <38213500+LinkLiar@users.noreply.github.com>
2022-02-17 21:19:43 +0800

    Update Generate.py

--- Generate.py
+++ Generate.py
@@ -1,63 +1,53 @@
-from os import error
 import random
 import string
-maxSize = 30
-generateTime = 1000
-characters = string.ascii_uppercase + string.digits + string.ascii_lowercase
-f = open('sample.txt', 'w')
-def GenerateRandomStrings(count , char):    # 字符串错误量不超过正确结果字数的1/4
+
+minSize = 16    # 字符串长度上限
+maxSize = 50    # 字符串长度下限
+generateTime = 100000    # 生成多少基准字符串
+errorPerNchar = 8    # 控制基准字符串错误量上限
+rowPerString = 5    # 每个基准字符串生成多少错误字符串
+txtName = "sample.txt"
+
+# TO DO: 添加限制，使不能对基准字符串上的同个位置进行 rowPerString/2 以上次数的修改
+
+characters = string.ascii_uppercase + string.digits + string.ascii_lowercase    # 字符集 = 大写 + 数字 + 小写
+f = open(txtName, 'w')
+def GenerateRandomStrings(count , char):    # 字符串错误量不超过正确结果字数的1/n
     for i in range(0 , count):
-        size = random.randint(6, maxSize)
+        size = random.randint(minSize, maxSize)    # 随机生成基准字符串长度
         tempRaw = ''.join(random.sample(characters,size))
         f.write(tempRaw + '\n')
         tempRaw = list(tempRaw)
-        print("correct:" + str(size))
-        for _ in range(0,5):
+        for _ in range(0,rowPerString):
             temp = tempRaw.copy()
-            errorTime = random.randint(0, int(size/6))
-            print("correct:" + str(size))
+            errorTime = random.randint(0, int(size/errorPerNchar))
             pos = []
             for _ in range(0, errorTime):
                 if(len(temp) != 1):
                     errorType = random.randint(0,2)
                 else:
                     errorType = random.randint(0,1)
-                if (errorType == 0):
+                if (errorType == 0):    # 修改一个字符
                     revise(temp,pos)
-                elif (errorType == 1):
+                elif (errorType == 1):    # 添加一个字符
                     add(temp)
-                elif (errorType == 2):
+                elif (errorType == 2):    # 删除一个字符
                     delete(temp)
             f.write(''.join(temp) + '\n')
         f.write('\n')
 
-def revise(temp,pos):
+def revise(temp,pos):    # 修改一个字符
     p = random.randint(0,len(temp)-1)
-    # while(p in pos):
-    #     p = random.randint(0,len(temp)-1)
-    #     if(len(temp)<3 & len(pos)>1):
-    #         break
-    #     print(p) 
-    temp[p]=characters[random.randint(0,len(characters)-1)]
+    temp[p] = characters[random.randint(0,len(characters)-1)]
     pos.append(p)
 
-def add(temp):
+def add(temp):    # 添加一个字符
     p = random.randint(0,len(temp)-1)
     temp.insert(p,characters[random.randint(0,len(characters)-1)])
 
-def delete(temp):
+def delete(temp):    # 删除一个字符
     p = random.randint(0,len(temp)-1)
     temp.pop(p)
     
-    
-# temp = list("abcabcabcabcabc")
-# pos = []
-# revise(temp,pos)
-GenerateRandomStrings(1000, characters)
-
-# # print(choices)
-# # optional shuffle, since they are produced in order of `chars`
-# random.shuffle(choices)
-# # Test: output how many distinct values there are:
-# print(choices[1])
-# print (len(choices))
+if __name__ == '__main__':
+    GenerateRandomStrings(generateTime, characters)
